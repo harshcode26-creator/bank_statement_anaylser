4fccd97bd16e31ad9815d4b105c332ea0b8ff2d7
Harsh Prajapati <harshcode26@gmail.com>
2026-04-22 16:37:57 +0530

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://finnova-4fjbelsm0-harshcode26-creators-projects.vercel.app"
+    "https://finnova-4fjbelsm0-harshcode26-creators-projects.vercel.app",
+    "https://finnova-livid.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
